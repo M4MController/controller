ddaacd27e45138de203a26bc341952da1e427382
Ed Asriyan <ed-asriyan@protonmail.com>
2019-02-10 15:27:47 +0000

    Update api

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const SENSOR_ID = 1;
 
 const config = require('./config');
-const http = require('http');
+const fetch = require('node-fetch');
 const Meter = require('./meter');
 
 const meter = new Meter('/dev/ttyACM0');
-
+console.log("init", new Date().toISOString());
 const data = meter.get(SENSOR_ID);
-
+let date = new Date();
+date.setHours(date.getHours() + 3);
 const body = JSON.stringify({
     'controller_mac': config['controller']['mac'],
     'sensor_id': SENSOR_ID,
-    'value': data['Value'],
-    'timestamp': new Date().toISOString().substr(0, 19), // there is a bug in libmbus
+    'value': data['Value'] * 10,
+    'hash': date.toString(),
+    'timestamp': date.toISOString().substr(0, 19), // there is a bug in libmbus
 });
-http.request({
-    host: config['server']['host'],
-    path: `${config['server']['prefix']}/sensor.addRecord`,
-    port: config['server']['port'],
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': body.length
-    },
-}).write(body);
+console.log(JSON.stringify(data))
+console.log(body);
+fetch(`${config['server']['host']}${config['server']['prefix']}/sensor.addRecord`,
+    {
+        method: 'POST',
+        body: body,
+//        headers: { 'Content-Type': 'application/json' },
+    })
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(e => console.log(e));
+
