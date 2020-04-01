899c07a54a9c9d19a9029467c06367f822cb7df1
Ed Asriyan <ed-asriyan@protonmail.com>
2020-04-01 22:56:44 +0300

    Create docker deploy

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:alpine
+MAINTAINER Ed Asriyan <ed-asriyan@protonmail.com>
+
+RUN apk update && apk add --no-cache autoconf automake cmake g++ git libtool make
+RUN git clone https://github.com/samkrew/libmbus && \
+    cd libmbus && \
+    autoheader && aclocal && libtoolize --ltdl --copy --force && automake --add-missing --copy && autoconf && \
+    ./configure && make && make install && \
+    cd .. && rm -fr libmbus
+
+WORKDIR /application
+
+ADD package.json .
+ADD meter ./meter
+RUN cd meter && cmake CMakeLists.txt && make && npm install
+
+ADD config.json .
+ADD index.js .
+
+CMD npm start -- --sensor-id $SENSOR_ID --interval $INTERVAL --device $DEVICE
--- index.js
+++ index.js
@@ -1,30 +1,62 @@
-const SENSOR_ID = 1;
-
 const config = require('./config');
 const fetch = require('node-fetch');
-const Meter = require('./meter');
+let Meter = require('./meter');
+const {ArgumentParser} = require('argparse');
 
-const meter = new Meter('/dev/ttyACM0');
-console.log("init", new Date().toISOString());
-const data = meter.get(SENSOR_ID);
-let date = new Date();
-date.setHours(date.getHours() + 3);
-const body = JSON.stringify({
+const USE_STUBS = process.env['USE_STUBS'];
+
+if (USE_STUBS) {
+  Meter = class {
+    _value = 100;
+
+    get() {
+      return this._value += Math.round(Math.random() * 10);
+    }
+  };
+}
+
+const sendValue = function(sensorId, value) {
+  const date = new Date();
+  const body = JSON.stringify({
     'controller_mac': config['controller']['mac'],
-    'sensor_id': SENSOR_ID,
+    'sensor_id': sensorId,
     'value': data['Value'] * 10,
     'hash': date.toString(),
     'timestamp': date.toISOString().substr(0, 19), // there is a bug in libmbus
-});
-console.log(JSON.stringify(data))
-console.log(body);
-fetch(`${config['server']['host']}${config['server']['prefix']}/sensor.addRecord`,
-    {
+  });
+  return fetch(
+      `${config['server']['host']}${config['server']['prefix']}/sensor.addRecord`,
+      {
         method: 'POST',
         body: body,
-//        headers: { 'Content-Type': 'application/json' },
-    })
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(e => console.log(e));
+        // headers: { 'Content-Type': 'application/json' },
+      });
+};
 
+const main = function() {
+  const parser = new ArgumentParser({
+    description: 'Animated stickers for Telegram (*.tgs) to animated GIFs converter',
+  });
+  parser.addArgument('--sensor-id', {type: Number, required: true});
+  parser.addArgument('--device', {required: true});
+  parser.addArgument('--interval', {type: Number, default: 10});
+
+  const args = parser.parseArgs();
+  const meter = new Meter(args.device);
+
+  setInterval(() => {
+    try {
+      const data = meter.get(1); // mbus sensor id = 1
+      if (data && data['Value']) {
+        console.log(data);
+      } else {
+        console.error('Can not read value');
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }, args.interval * 1000);
+
+};
+
+main();
